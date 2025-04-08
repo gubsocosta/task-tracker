f19fdee1d80a16511ea9610e24fe7a633ca0b06e
Gabriel Costa <gubsocosta@gmail.com>
2025-04-08 18:00:13 -0300

    feat: create sidebar and form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>Alura Tracker</h1>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+    <div class="column is-three-quarter">
+      <Form />
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
+import Form from '@/components/Form.vue';
+import SideBar from '@/components/SideBar.vue';
 </script>
 
-<style scoped lang="scss">
+<style scoped lang="sass">
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+          class="column is-8"
+          role="form"
+          aria-label="Form to create a new task"
+      >
+        <input type="text" class="input" placeholder="What task you want to create?"/>
+      </div>
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>{{ elapsedTime }}</strong>
+          </section>
+          <button class="button" @click="startStopWatcher">
+            <span class="icon">
+              <i class="fa fa-play"></i>
+            </span>
+            <span>Play</span>
+          </button>
+          <button class="button" @click="stopStopWatcher">
+            <span class="icon">
+              <i class="fa fa-stop"></i>
+            </span>
+            <span>Stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const stopWatcher = ref(0);
+const timeInSeconds = ref(0);
+const elapsedTime = computed(() => new Date(timeInSeconds.value * 1000).toISOString().substring(11,19));
+
+function startStopWatcher () {
+  stopWatcher.value = setInterval(() => {
+    timeInSeconds.value += 1;
+  }, 1000)
+}
+
+function stopStopWatcher () {
+  clearInterval(stopWatcher.value);
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
